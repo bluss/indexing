bb7894bdc115f01717ed5685219068715dc71bf4
bluss <bluss@users.noreply.github.com>
2016-10-31 18:13:49 +0100

    Add more compile fail tests

---
+++ tests/compile-fail/basic-branding-pointers.rs
@@ -0,0 +1,20 @@
+extern crate indexing;
+
+use indexing::scope;
+
+fn main() {
+    let arr1 = [1, 2, 3, 4, 5];
+
+    // can hold onto the pointers for later, as long they stay in the closure
+    let _a = scope(&arr1[..], |arr| {
+        let r = arr.pointer_range();        //~ ERROR cannot infer an appropriate lifetime
+        let r = r.nonempty().unwrap();      
+        let i = r.first();
+        println!("{}", &arr[i]);
+
+        // should be invalid to return an index
+        i
+    });
+}
+
+
