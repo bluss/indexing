fe6fb6cdde957d53643e5ba3b5174416499629f7
bluss <bluss@users.noreply.github.com>
2016-10-31 18:13:48 +0100

    Improve performance of scan_tail

--- pointer.rs
+++ pointer.rs
@@ -157,14 +157,18 @@ impl<'id, T, Array> Container<'id, Array> where Array: Buffer<Target=[T]> {
         where F: FnMut(&T) -> bool
     {
         unsafe {
-            let mut start = index.ptr();
-            for elt in self[..index].iter().rev() {
-                if !f(elt) {
+            let container_start = self.pointer_range().start;
+            let mut end = index.ptr();
+            loop {
+                if !f(&*end.offset(-1)) {
                     break;
                 }
-                start = elt as *const _;
+                if container_start == end {
+                    break;
+                }
+                end.dec();
             }
-            PRange::from(start, index.ptr().offset(1))
+            PRange::from(end, index.ptr().offset(1))
         }
     }
 }
@@ -225,3 +229,47 @@ impl<'id, T> DoubleEndedIterator for PRange<'id, T> {
     }
 }
 
+// Copyright 2016 bluss
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+/// Extension methods for raw pointers
+pub trait PointerExt : Copy {
+    unsafe fn offset(self, i: isize) -> Self;
+
+    /// Increment by 1
+    #[inline(always)]
+    unsafe fn inc(&mut self) {
+        *self = self.offset(1);
+    }
+
+    /// Decrement by 1
+    #[inline(always)]
+    unsafe fn dec(&mut self) {
+        *self = self.offset(-1);
+    }
+
+    /// Offset by `s` multiplied by `index`.
+    #[inline(always)]
+    unsafe fn stride_offset(self, s: isize, index: usize) -> Self {
+        self.offset(s * index as isize)
+    }
+}
+
+impl<T> PointerExt for *const T {
+    #[inline(always)]
+    unsafe fn offset(self, i: isize) -> Self {
+        self.offset(i)
+    }
+}
+
+impl<T> PointerExt for *mut T {
+    #[inline(always)]
+    unsafe fn offset(self, i: isize) -> Self {
+        self.offset(i)
+    }
+}
